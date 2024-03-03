6ec3d0c4553be0cfa3d6f62de2fff69bae5775e2
Aryan Jain <aryanofficialjain@gmail.com>
2024-03-03 12:06:10 +0530

    mongo db atlas is sucessfully connected

--- server/index.js
+++ server/index.js
@@ -2,13 +2,34 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require('body-parser');
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.get("/", (req,res)=> {
-    res.send("HOMEPAGE");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+
+
+
+const dbURI = "mongodb+srv://aryanofficialjain:7985447692@cluster0.i9we7he.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+
+mongoose.connect(dbURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 })
-
-
-app.listen(PORT, ()=> {
-    console.log("RUNNING !!!!!!");
+.then(() => {
+  console.log("Connected to MongoDB Atlas");
 })
+.catch((err) => {
+  console.error("Error connecting to MongoDB Atlas:", err.message);
+});
+
+
+app.get("/", (req, res) => {
+  res.send("HOMEPAGE");
+});
+
+
+app.listen(PORT, () => {
+  console.log("Server is running on port " + PORT);
+});
